e54ecc6f1adcc40092c346916b33700b92a19cdf
LordM8YT <77118104+LordM8YT@users.noreply.github.com>
2026-07-29 17:32:26 +0200

    fix: prevent HUD overlap at compact resolutions

--- resources/[fluxcore]/fluxcore_status/client/main.lua
+++ resources/[fluxcore]/fluxcore_status/client/main.lua
@@ -229,7 +229,7 @@ local function configureMinimap()
         'L',
         'B',
         -0.0045,
-        -0.047,
+        -0.060,
         0.150,
         0.188888
     )
@@ -238,7 +238,7 @@ local function configureMinimap()
         'L',
         'B',
         0.020,
-        0.007,
+        -0.006,
         0.111,
         0.159
     )
@@ -247,7 +247,7 @@ local function configureMinimap()
         'L',
         'B',
         -0.030,
-        -0.003,
+        -0.016,
         0.266,
         0.237
     )
